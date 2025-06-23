b827bd3cb7a9fa7256d55db2da86ce1c1b5b0827
Mariano Fernández <marianofernandez587@gmail.com>
2025-06-23 16:33:40 -0300

    acentos

--- app.py
+++ app.py
@@ -10,24 +10,24 @@ app = Flask(__name__)
 def enviar():
     #TOKEN DE ACCESO DE FACEBOOK
     token='EAA6LJPGCxHsBOzEyjcFZB7q5XZBfBEBLCEojLBZBjpCf8Sj34lmBtUOYdD6tZBs1OymHZB2OFd5Vvq0nZB6DGZCoPec7QDl2jkvnNeXezFA9iUq89KDH8v7wvKPP2U9Tdh7XQNu7FYZBiNPuBxCnGyLZBj5yCg0Yk1dY4dqiivvuZAikz5x5EVCoQZAZBvpM7vMEVqr7V8Ns181D0QFUsJcNVDwwuyBmPpmxKAIDO9f6cNCwC2cmDwZDZD'
-    #IDENTIFICADOR DE NÚMERO DE TELÉFONO
-    idNumeroTeléfono='651005361434016'
-    #TELEFONO QUE RECIBE (EL DE NOSOTROS QUE DIMOS DE ALTA)
-    telefonoEnvia='541125123781'
+    #IDENTIFICADOR DE NUMERO DE TELEFONO  <-- Comentario sin acento
+    idNumeroTelefono='651005361434016'  # <-- Variable sin acento
+    #TELEFONO QUE RECIBE (EL DE NOSOTROS QUE DIMOS DE ALTA) <-- Comentario sin acento
+    telefonoEnvia='541125123781'      # <-- Variable sin acento
     #MENSAJE A ENVIAR
     textoMensaje="Hola novato saludos"
     #URL DE LA IMAGEN A ENVIAR
     urlImagen='https://i.imgur.com/r5lhxgn.png'
 
     #INICIALIZAMOS ENVIO DE MENSAJES
-    mensajeWa=WhatsApp(token,idNumeroTeléfono)
+    mensajeWa=WhatsApp(token, idNumeroTelefono) # <-- Uso de la variable sin acento
     #ENVIAMOS UN MENSAJE DE TEXTO
-    mensajeWa.send_message(textoMensaje,telefonoEnvia)
+    mensajeWa.send_message(textoMensaje, telefonoEnvia)
     #ENVIAMOS UNA IMAGEN
-    mensajeWa.send_image(image=urlImagen,recipient_id=telefonoEnvia,)
+    mensajeWa.send_image(image=urlImagen, recipient_id=telefonoEnvia)
     return "mensaje enviado exitosamente"
 
 
-#INICIAMSO FLASK
+#INICIAMOS FLASK
 if __name__ == "__main__":
     app.run(debug=True)
